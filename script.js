9dcc446e87ab68f7df4cb4f12d8ff64fdc69027c
DevinMprz <denistanasuk@gmail.com>
2025-01-25 22:05:31 +0100

    init

---
+++ script.js
@@ -0,0 +1,12 @@
+import { createApp, ref, onMounted } from 'vue'
+import App from './App.vue'
+
+const notes = ref([
+    {
+    note: 'citrus'
+    color,
+    },])
+
+const app = createApp({
+
+})
